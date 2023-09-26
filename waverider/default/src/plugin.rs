d2079226931a465120c152df11eff7e741abeb19
Carlos <75177443+0xcen@users.noreply.github.com>
2023-09-26 21:04:55 +0200

    first commit

--- waverider/default/src/plugin.rs
+++ waverider/default/src/plugin.rs
@@ -1,9 +1,22 @@
+use async_std::os::unix::net::UnixDatagram;
 use postgrest::Postgrest;
 use serde::Deserialize;
-use solana_geyser_plugin_interface::geyser_plugin_interface::GeyserPluginError;
+use solana_client::rpc_config::{RpcAccountInfoConfig, RpcProgramAccountsConfig};
+use solana_client::rpc_filter::{Memcmp, MemcmpEncodedBytes, RpcFilterType};
 use solana_geyser_plugin_interface::geyser_plugin_interface::{
     GeyserPlugin, ReplicaAccountInfoVersions, Result as PluginResult,
 };
+use solana_geyser_plugin_interface::geyser_plugin_interface::{
+    GeyserPluginError, ReplicaAccountInfo,
+};
+
+use solana_account_decoder::UiAccountEncoding;
+
+use solana_client::rpc_client::RpcClient;
+use solana_program::pubkey;
+use solana_sdk::client;
+use solana_sdk::commitment_config::CommitmentConfig;
+
 use std::{
     error::Error,
     fmt::{self, Debug},
@@ -15,14 +28,13 @@ use tokio::runtime::Runtime;
 pub struct SupabasePlugin {
     postgres_client: Option<Postgrest>,
     configuration: Option<Configuration>,
-    programs: Vec<[u8; 32]>,
+    accounts: Vec<[u8; 32]>,
 }
 
 #[derive(Clone, Debug, Deserialize)]
 pub struct Configuration {
     pub supabase_url: String,
     pub supabase_key: String,
-    pub programs: Option<Vec<String>>,
 }
 
 impl Configuration {
@@ -40,7 +52,7 @@ impl Default for SupabasePlugin {
         SupabasePlugin {
             postgres_client: None,
             configuration: None,
-            programs: Vec::new(),
+            accounts: Vec::new(),
         }
     }
 }
@@ -51,8 +63,39 @@ impl GeyserPlugin for SupabasePlugin {
     }
 
     fn on_load(&mut self, config_file: &str) -> PluginResult<()> {
+        // todo: get trigger account bytes
+        let TRIGGER_ACCOUNT_DISCRIMINATOR: Vec<u8> = vec![77, 155, 35, 144, 38, 14, 106, 88];
+
+        let connection = RpcClient::new_with_commitment(
+            "https://global.rpc.hellomoon.io/5d77d2b5-8b58-4131-95d7-ba9c59499a1c".to_string(),
+            CommitmentConfig::confirmed(),
+        );
+
+        let filters = Some(vec![RpcFilterType::Memcmp(Memcmp {
+            offset: 0,
+            bytes: MemcmpEncodedBytes::Bytes(TRIGGER_ACCOUNT_DISCRIMINATOR),
+            encoding: None,
+        })]);
+
+        let accounts = connection
+            .get_program_accounts_with_config(
+                &pubkey!("41NuR2mieT98yDQpXmwDzBZ24sz9UMAieorCr8Mw9C8Q"),
+                RpcProgramAccountsConfig {
+                    with_context: Some(false),
+                    filters,
+                    account_config: RpcAccountInfoConfig {
+                        commitment: Some(connection.commitment()),
+                        encoding: Some(UiAccountEncoding::Base64),
+                        ..RpcAccountInfoConfig::default()
+                    },
+                },
+            )
+            .unwrap();
+
+        println!("TriggrAccounts: {:#?}", accounts[0]);
+
         println!("config file: {}", config_file);
-        let config = match Configuration::load(config_file) {
+        let mut config = match Configuration::load(config_file) {
             Ok(c) => c,
             Err(_e) => {
                 return Err(GeyserPluginError::ConfigFileReadError {
@@ -60,21 +103,16 @@ impl GeyserPlugin for SupabasePlugin {
                 });
             }
         };
+
         println!("Your supabase url: {:#?} ", &config.supabase_url);
         self.postgres_client = Some(
             Postgrest::new(&config.supabase_url).insert_header("apikey", &config.supabase_key),
         );
 
-        match config.programs.as_ref() {
-            Some(accounts) => {
-                accounts.iter().for_each(|account| {
-                    let mut acc_bytes = [0u8; 32];
-                    acc_bytes.copy_from_slice(&bs58::decode(account).into_vec().unwrap()[0..32]);
-                    self.programs.push(acc_bytes);
-                });
-            }
-            None => (),
-        }
+        self.accounts = accounts
+            .iter()
+            .map(|account| account.0.to_bytes())
+            .collect();
 
         self.configuration = Some(config);
         Ok(())
@@ -89,43 +127,53 @@ impl GeyserPlugin for SupabasePlugin {
         is_startup: bool,
     ) -> PluginResult<()> {
         let account_info = match account {
-            ReplicaAccountInfoVersions::V0_0_1(_) => {
+            ReplicaAccountInfoVersions::V0_0_1(account_info) => account_info,
+            ReplicaAccountInfoVersions::V0_0_2(_) => {
                 return Err(GeyserPluginError::AccountsUpdateError {
                     msg: "V1 not supported, please upgrade your Solana CLI Version".to_string(),
                 });
             }
-            ReplicaAccountInfoVersions::V0_0_2(account_info) => account_info,
         };
 
-        self.programs.iter().for_each(|program| {
+        println!("Account owner: {:#?}", account_info.owner.len());
+        println!(
+            "Owner pubkey: {:#?}",
+            pubkey!("41NuR2mieT98yDQpXmwDzBZ24sz9UMAieorCr8Mw9C8Q").to_bytes()
+        );
 
-            if program == account_info.owner {
-                let account_pubkey = bs58::encode(account_info.pubkey).into_string();
-                let account_owner = bs58::encode(account_info.owner).into_string();
-                let account_data = account_info.data;
-                let _account_lamports = account_info.lamports;
-                let account_executable = account_info.executable;
-                let _account_rent_epoch = account_info.rent_epoch;
-                
+        if pubkey!("41NuR2mieT98yDQpXmwDzBZ24sz9UMAieorCr8Mw9C8Q").to_bytes() == account_info.owner
+        {
+            println!("conditions met");
+        };
 
-                let rt = Runtime::new().unwrap();
-                let result = rt.block_on(
-                    self.postgres_client
-                        .as_mut()
-                        .unwrap()
-                        .from("accounts")
-                        .upsert(
-                            serde_json::to_string(
-                                &serde_json::json!([{ "account": account_pubkey, "owner": account_owner, "data": account_data, "executable": account_executable }]),
-                            )
-                            .unwrap(),
-                        )
-                        .execute(),
-                );
-                println!("result: {:#?}, startup: {:#?}", result, is_startup);
-            } else {
-            }
-        });
+        // self.accounts.iter().for_each(|account| {
+
+        //     if account == account_info.owner {
+        //         let account_pubkey = bs58::encode(account_info.pubkey).into_string();
+        //         let account_owner = bs58::encode(account_info.owner).into_string();
+        //         let account_data = account_info.data;
+        //         let _account_lamports = account_info.lamports;
+        //         let account_executable = account_info.executable;
+        //         let _account_rent_epoch = account_info.rent_epoch;
+
+        //         let rt = Runtime::new().unwrap();
+        //         let result = rt.block_on(
+        //             self.postgres_client
+        //                 .as_mut()
+        //                 .unwrap()
+        //                 .from("accounts")
+        //                 .upsert(
+        //                     serde_json::to_string(
+        //                         &serde_json::json!([{ "account": account_pubkey, "owner": account_owner, "data": account_data, "executable": account_executable }]),
+        //                     )
+        //                     .unwrap(),
+        //                 )
+        //                 .execute(),
+        //         );
+        //         println!("result: {:#?}, startup: {:#?}", result, is_startup);
+        //     } else {
+        //     }
+        // });
 
         Ok(())
     }
